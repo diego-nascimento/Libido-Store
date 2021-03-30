c9650dffbee47c463f859f666f8ab00254f13826
Diego-Nascimento <diegostealth3@gmail.com>
2021-03-30 10:21:50 -0300

    padding

--- src/Components/ShowProducts/Produtos.style.ts
+++ src/Components/ShowProducts/Produtos.style.ts
@@ -78,11 +78,9 @@ export const SideBar = styled.aside`
   width: 100%;
   height: 100%;
   border: 1px solid ${styles.dest2Components};
-  padding: 0px 10px;
+  padding: 10px 10px;
 
-  .block{
-    border-bottom: 1px solid rgba(0,0,0, .5);
-
+  .block{  
     h3{
       text-decoration: underline;
     }
@@ -99,6 +97,7 @@ export const SideBar = styled.aside`
         }
       }
     }
+
   }
 
 `;
