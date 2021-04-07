0b3efb3f7b0d1bae19eb98a889fb6b7d93625609
Diego-Nascimento <diegostealth3@gmail.com>
2021-04-07 08:30:48 -0300

    add google statistic

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -55,6 +55,14 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
             integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
           />
+         <script dangerouslySetInnerHTML={
+            { __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments)}
+              gtag("js", new Date());
+              gtag("config", "G-MG5M9X4JLP");
+            `}
+          }/>
         </Head>
         <body>
           <Main />
