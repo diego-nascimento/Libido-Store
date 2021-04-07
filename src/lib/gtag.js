298756cabefd23d73956f1125247be3fc078eca2
Diego-Nascimento <diegostealth3@gmail.com>
2021-04-07 15:36:53 -0300

    e la vamos nois

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -2,17 +2,21 @@ export const GA_TRACKING_ID = 'G-00GF13YP96';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
+  window &&
+    window.gtag &&
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 
 export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  window &&
+    window.gtag &&
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
 };
