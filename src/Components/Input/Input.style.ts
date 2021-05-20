5587ea9959d3e00fcb590f526034d58c1ec52707
diego-nascimento <diegostealth3@gmail.com>
2021-05-20 15:44:52 -0300

    resolvendo conflito

--- src/Components/Input/Input.style.ts
+++ src/Components/Input/Input.style.ts
@@ -2,27 +2,6 @@ import styled from 'styled-components'
 import { styles } from '../../styles/styles'
 
 
-<<<<<<< HEAD
-export const InputComponent = styled.input`
-   width: 100%;
-  height: 40px;
-  padding: 10px;
-  border-radius: 15px;
-  background-color: #ddd;
-  border: none;
-  font-size: .9rem;
-  font-weight: 500;
-  margin: 5px 0px;
-  color: ${styles.componentsDest};
-
-  ::placeholder{
-    color: ${styles.componentsDest};
-  }
-`
-
-export const Container = styled.div`
-  display: flex;
-=======
 interface IContainer{
   borderColor?: string | null
   show?: boolean
@@ -31,7 +10,6 @@ interface IContainer{
 
 export const Container = styled.div<IContainer>`
   display: ${props => props.show === true ? 'flex' : 'none'};
->>>>>>> pagamentos
   flex-direction: column;
   width: 100%;
    
