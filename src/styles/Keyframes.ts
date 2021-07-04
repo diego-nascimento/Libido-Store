2992a3feb84b01f65d6c42754813ce542277baf8
diego-nascimento <diegostealth3@gmail.com>
2021-07-04 01:59:46 -0300

    build: testes

--- src/styles/Keyframes.ts
+++ src/styles/Keyframes.ts
@@ -1,22 +1,24 @@
-import { keyframes } from 'styled-components';
-
+import { keyframes } from 'styled-components'
 
 interface IImageShowUp{
   opacity: number
 }
 
-export const ImageShowUp = ({opacity}:IImageShowUp)  => keyframes`
+export const ImageShowUp = ({ opacity }:IImageShowUp) => keyframes`
   from{
     opacity: 0
   }to{
     opacity: ${opacity};
   }
-`;
+`
 
 export const PBottomTop = keyframes`
   from
     transform: translateY(-20px)
   }to{
+
+
+    
     transform: translateY(0px);
   }
-`;
+`
